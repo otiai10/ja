34d2a7253bd9f135f80813d8635912936ea4f995
Hiromu OCHIAI <otiai10@gmail.com>
2021-08-02 15:27:07 +0900

    Add weekday

---
+++ weekday.go
@@ -0,0 +1,13 @@
+package ja
+
+import "time"
+
+var Weekday = map[time.Weekday]string{
+	time.Sunday:    "日",
+	time.Monday:    "月",
+	time.Tuesday:   "火",
+	time.Wednesday: "水",
+	time.Thursday:  "木",
+	time.Friday:    "金",
+	time.Saturday:  "土",
+}
